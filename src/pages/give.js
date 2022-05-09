73b88157463c8ce7aa7156bd3edaa4336a931bb9
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-05-09 22:34:13 +0000

    give update #2

--- src/pages/give.js
+++ src/pages/give.js
@@ -5,6 +5,7 @@ import { exchangeRates, currencies } from "exchange-rates-api"
 import momo from '../images/momo.png'
 import card from '../images/card.png'
 import paypal from '../images/paypal.png'
+import give from '../images/give2.jpg'
 
 import { Momo } from '../components/payment/momo'
 import { CreditCard } from '../components/payment/credit'
@@ -49,12 +50,10 @@ const Give = () => {
     return (
         <div className='h-screen w-screen flex justify-center overflow-x-hidden'>
             <div className='lg:w-1/2 h-full'>
-                <div className="relative bgDark h-32">
+                <div className="relative bgDark h-72">
                     <div className="absolute w-full h-full z-10 flex items-center justify-center">
-                        <p className='text-4xl lg:text-6xl font-bold text-white'>GIVE</p>
                     </div>
-                    <div className="absolute bg-red-800 h-32 w-full opacity-40 flex items-center justify-center"></div>
-                    <img src='https://images.unsplash.com/photo-1506755594592-349d12a7c52a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y3Jvd2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' className="h-32 w-full object-cover object-center" alt="" />
+                    <img src={give} className="h-72 w-full object-cover object-center" alt="" />
                 </div>
 
                 <div className='py-8 grid grid-cols-3 w-full px-2 lg:px-32 gap-2 lg:gap-4'>
