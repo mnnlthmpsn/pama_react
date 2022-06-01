972e074cff793ee81e649742e86637604bdfafb9
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-06-01 13:12:09 +0000

    partner

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,30 +1,30 @@
 import { ChevronLeftIcon } from "@heroicons/react/solid"
 import { useNavigate } from "react-router-dom"
-import { Button } from '../components/components'
 import FlatPickr from 'react-flatpickr'
 import { useState } from "react"
 
 const Checkout = () => {
 
     const router = useNavigate()
-    const [ date, setDate ] = useState(() => new Date())
+    const [date, setDate] = useState(() => new Date())
 
     return (
         <div className="flex flex-col px-8 pt-8 overflow-x-hidden">
             <div className="flex justify-start" onClick={() => router(-1)}>
                 <ChevronLeftIcon className="h-10 mb-4 border rounded-full p-2" />
             </div>
-            <p className="text-2xl pb-8">Checkout</p>
+            <p className="text-xl font-bold pb-8">To Partner with Chris Asante Ministries - please fill out the form below and sow
+                your seed to Activate your Covenant Partnership</p>
             <p className="text-xl font-light pb-6">Platinum Partner</p>
 
             {/* date picker */}
             <div className="flex flex-col space-y-2 pb-3 font-light">
                 <label htmlFor="date_picker">Start Date</label>
                 {/* <input type="date" className="px-3 py-2 border-2 border" /> */}
-                <FlatPickr 
-                className="px-3 py-2 outline-none border w-full bg-white"
+                <FlatPickr
+                    className="px-3 py-2 outline-none border w-full bg-white"
                     value={date}
-                    onchange={([ date ]) => setDate(date)}
+                    onchange={([date]) => setDate(date)}
                 />
             </div>
 
@@ -44,19 +44,66 @@ const Checkout = () => {
             <p className="py-5 font-light border-b">You will be charged monthly until canceled</p>
 
             <div className="pt-8 space-y-5">
-                <p className="text-2xl">1. Sign up</p>
-                <p className="font-light">
-                    To purchase this plan and use its benefits in the future, log in to your account or sign up
-                </p>
-            </div>
+                <p className="text-xl">Select Payment Method</p>
+                <div className="flex font-light flex-col space-y-2">
+                    <label className="flex space-x-3 items-center">
+                        <input type="radio" name="payment_method" />
+                        <p>Debit/Credit Card</p>
+                    </label>
 
-            <div className="flex flex-col py-8 border-b space-y-2">
-                <Button label='Sign Up' />
-                <Button label='Login' isSecondary={true} />
+                    <label className="flex space-x-3 items-center">
+                        <input type="radio" name="payment_method" />
+                        <p>Mobile Money</p>
+                    </label>
+
+                    <label className="flex space-x-3 items-center">
+                        <input type="radio" name="payment_method" />
+                        <p>Paypal</p>
+                    </label>
+                </div>
             </div>
 
             <div className="pt-8 space-y-5">
-                <p className="text-2xl">2. Payment</p>
+                <p className="text-xl">Personal Info</p>
+                <form className="space-y-8">
+                    {/* title */}
+                    <div className="flex flex-col space-2 items-start">
+                        <label htmlFor="title">Title</label>
+                        <select name="title" id="title" className="form-control">
+                            <option value="MR">MR</option>
+                            <option value="MR">MRS</option>
+                            <option value="MR">MISS</option>
+                        </select>
+                    </div>
+
+                    {/* first name */}
+                    <div className="flex flex-col space-2 items-start">
+                        <label htmlFor="title">First name</label>
+                        <input type="text" name="first_name" id="first_name" className="form-control" />
+                    </div>
+
+                    {/* last name */}
+                    <div className="flex flex-col space-2 items-start">
+                        <label htmlFor="title">Last name</label>
+                        <input type="text" name="first_name" id="last_name" className="form-control" />
+                    </div>
+
+                    {/* Phone */}
+                    <div className="flex flex-col space-2 items-start">
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" name="phone" id="phone" className="form-control" />
+                    </div>
+
+                    {/* Email */}
+                    <div className="flex flex-col space-2 items-start">
+                        <label htmlFor="title">Email</label>
+                        <input type="text" name="email" id="email" className="form-control" />
+                    </div>
+
+                    <div className="pb-4">
+                        <button className="btn-primary">Activate Covenant</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
