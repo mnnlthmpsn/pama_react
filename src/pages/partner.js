b3bd2d5d0d5d20961a212bfcb950f5df7ee5facb
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-06-18 01:49:50 +0000

    benefits

--- src/pages/partner.js
+++ src/pages/partner.js
@@ -46,76 +46,80 @@ const PartnerShipReason = () => {
     <div className="p-6 space-y-1">
       <div className="space-y-2">
         <p
-          className="text-xl font-semibold border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
+          className="font-semibold text-center uppercase border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
           onClick={() => setActive("1")}
         >
-          Benefits
+          Vision
         </p>
         <p
-          className={`text-lg duration-300 ${
-            active === "1" ? "opacity-100" : "opacity-0 h-0"
-          }`}
+          className={`text-lg duration-300 ${active === "1" ? "opacity-100" : "opacity-0 h-0"
+            }`}
         >
-          The vision of this ministry is to use every channel available to bring
-          hope to people all over the world, to make a difference in the lives
-          of those we touch with the transforming healing power of Christ. To
-          reach the lost, encourage and equip believers and spread the good news
-          of Jesus Christ is the purpose of Ed Citronnelli Ministries. Through
-          your faithful and consistent support, you become an expression of
-          Christ’s love for people. When you become a covenant partner with us
-          as a world healer, you share the blessings, the eternal rewards and
-          the fulfillment of what God is doing throughout the earth. Miracles
-          happen for others and for you. Because of your generosity, God’s favor
-          and goodness become a part of your life and your whole household.
+          The vision of this ministry is to use every channel available to bring hope to people
+          all over the world, to make a difference in the lives of those we touch with the
+          transforming healing power of Christ. To reach the lost, encourage and equip believers
+          and spread the good news of Jesus Christ is the purpose of  Chris Asante Ministries.
+          Through your faithful and consistent support, you become an expression of Christ’s love for people
         </p>
       </div>
+
+      {/* why become a partner */}
       <div className="space-y-2">
         <p
-          className="text-xl font-semibold border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
+          className="text-center font-semibold uppercase space-y-2 border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
           onClick={() => setActive("2")}
         >
-          Why Partner?
+          Why should I become a Partner?
         </p>
         <p
-          className={`text-lg duration-300 ${
-            active === "2" ? "opacity-100" : "opacity-0 h-0"
-          }`}
+          className={`text-lg duration-300 ${active === "2" ? "opacity-100" : "opacity-0 h-0"
+            }`}
         >
-          As a World Healer, you partner with Pastor Ed and the Holy Spirit to
-          heal the nations. Your heart and hands are joined with them by praying
-          and financially supporting Ed Citronnelli Ministries each month. The
-          amount of your giving is between you and God. Giving is meant to be an
-          expression of your love and commitment to advancing the kingdom of
-          God. A World Healer is someone who is an extension of the healing
-          hands of Jesus Christ and a hero who comes to the rescue of those in
-          need. In our world today we need healers. People need hope and
-          healing, they need to discover the miracle of a personal relationship
-          with Jesus Christ
+          Who ever receives a prophet in the name of a prophet will receive the reward of a prophet.
+          The Gospel is free to all, but it is very expensive to take it to the nations.
+          When you become a covenant partner with us , you share the blessings, the eternal rewards and the fulfillment of what God is
+          doing throughout the earth. Miracles happen for others and for you. Because of your generosity, God’s favor and goodness 
+          become a part of your life and your whole household
+
+          Your decision to make the commitment to partner with me, with your generous monthly ministry gift is a covenant decision. 
+          God is a God of Covenants and Principles. Simply pray about it, and use one of the options below to decide what kind 
+          of Partnership level you feel led of the Holy Spirit to do
         </p>
       </div>
 
       <div className="space-y-2">
         <p
-          className="text-xl font-semibold border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
+          className="w-full h-full font-semibold text-center uppercase border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
           onClick={() => setActive("3")}
         >
-          Pastor's Fellowship
+          Who can become a partner
         </p>
-        <p
-          className={`text-lg duration-300 ${
-            active === "3" ? "opacity-100" : "opacity-0 h-0"
-          }`}
+        <ul
+          className={`px-10 flex flex-col space-y-4 duration-300 list-disc ${active === "3" ? "opacity-100" : "opacity-0 h-0"
+            }`}
         >
-          As a World Healer, you partner with Pastor Ed and the Holy Spirit to
-          heal the nations. Your heart and hands are joined with them by praying
-          and financially supporting Ed Citronnelli Ministries each month. The
-          amount of your giving is between you and God. Giving is meant to be an
-          expression of your love and commitment to advancing the kingdom of
-          God. A World Healer is someone who is an extension of the healing
-          hands of Jesus Christ and a hero who comes to the rescue of those in
-          need. In our world today we need healers. People need hope and
-          healing, they need to discover the miracle of a personal relationship
-          with Jesus Christ
+          <li>Anyone who believes in our Lord Jesus Christ and His Kingdom</li>
+          <li>Anyone who believes in CAM and what it stands for can become a partner</li>
+          <li>You don’t have to be a Pastor, Prophet, evangelist or preacher</li>
+          <li>Our Partners range from ordinary members of the community, COGA and non- COGA members</li>
+        </ul>
+      </div>
+
+      <div className="space-y-2">
+        <div
+          className="w-full h-full font-semibold text-center uppercase border bg-gray-100 rounded py-2 px-4 flex items-center justify-center"
+          onClick={() => setActive("4")}
+        >
+          What makes you a partner
+        </div>
+        <p
+          className={`text-lg duration-300 ${active === "4" ? "opacity-100" : "opacity-0 h-0"
+            }`}
+        >
+          Partners are individuals, and families, businesses and ministries that faithfully and periodically sow some level of 
+          financial support and prayer into Chris Asante Ministries. Your Partner gift enables CAM to carry out its global 
+          mission and positions you to receive the rewards for the work, the anointings of the ministry and a grand harvest 
+          on your seed sown
         </p>
       </div>
     </div>
@@ -134,7 +138,7 @@ const Subscriptions = () => {
       </p>
       <div className="grid gap-5">
         {data?.map((item) => (
-          <PartnerCard item={item} key={item.id}/>
+          <PartnerCard item={item} key={item.id} />
         ))}
       </div>
     </div>
